eb8a2173b30c9d01322160daa2ba699bced6e4f8
amjadkhan1995 <amjad.khan.contact@gmail.com>
2019-06-12 16:56:17 +0300

    update

--- src/app/movies/moives-list.component.ts
+++ src/app/movies/moives-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IMovie } from './movie';
+import { MoiveService } from './service';
 
 @Component({
   selector: 'app-moives-list',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoivesListComponent implements OnInit {
 
-  constructor() { }
+  errorMessage: string;
+  filteredMovies: IMovie[] =[];
+  movies: IMovie[] =[];
+  
 
-  ngOnInit() {
+  _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredMovies = this.listFilter ? this.performFilter(this.listFilter) : this.movies;
+  }
+
+  performFilter(filterBy: string): IMovie[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.movies.filter((movie: IMovie) =>
+    movie.movieName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  constructor(private moiveService: MoiveService) { }
+
+  ngOnInit(): void {
+    this.moiveService.getMovies().subscribe(
+      movie => { 
+        this.movies = movie,
+        this.filteredMovies = this.movies
+      },
+      error => this.errorMessage = <any>error
+      
+    );
   }
 
 }
---
+++ src/app/movies/service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { IMovie } from './movie';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class MoiveService{
+
+  public moviesAPI ='api/movies/movies.json';
+  
+  constructor(private http: HttpClient){
+
+  }
+
+  getMovies(): Observable<IMovie[]>{
+    console.log('getMovies() enter');
+    return this.http.get<IMovie[]>(this.moviesAPI).pipe(
+    tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+    
+  }
+  
+
+  private handleError(err: HttpErrorResponse) {
+    
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+}
